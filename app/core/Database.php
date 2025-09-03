81fa273782286c3170e6d2927d82630338fc8c46
iXP0Mt <alex.av.acc@gmail.com>
2025-09-03 15:15:49 +0500

    Set up database connection

---
+++ app/core/Database.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace app\core;
+
+use PDO;
+use PDOException;
+
+class Database
+{
+    private static function pdo(): PDO
+    {
+        static $pdo;
+        if(!$pdo)
+        {
+            $config = require 'config/database.php';
+            $dsn = 'mysql:dbname='.$config['db_name'].';host='.$config['db_host'].';charset=utf8';
+            try {
+                $pdo = new PDO($dsn, $config['db_user'], $config['db_pass']);
+                $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            } catch (PDOException $e) {
+                error_log("ERROR: " . $e->getMessage());
+            }
+        }
+        return $pdo;
+    }
+}
---
+++ config/database.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'db_name' => 'library_books_db',
+    'db_host'=> 'localhost',
+    'db_user'=> 'root',
+    'db_pass'=> '',
+];
