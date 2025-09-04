3b18c3dd8522329ba2fa1a7841c627ddbd7b38a5
iXP0Mt <alex.av.acc@gmail.com>
2025-09-04 18:29:03 +0500

    Set up php-dotenv

--- index.php
+++ index.php
@@ -4,4 +4,7 @@ ini_set('display_errors', 1);
 
 require __DIR__ . '/vendor/autoload.php';
 
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
 require_once 'router/routes.php';
