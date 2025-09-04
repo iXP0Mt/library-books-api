63b596ff447c3f48066e2c7466acd70724548715
iXP0Mt <alex.av.acc@gmail.com>
2025-09-04 17:39:50 +0500

    Added constants for length login and password

---
+++ app/util/Constants.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace app\util;
+
+class Constants
+{
+    public const MIN_LOGIN_LENGTH = 3;
+    public const MAX_LOGIN_LENGTH = 64;
+    public const MIN_PASSWORD_LENGTH = 8;
+    public const MAX_PASSWORD_LENGTH = 64;
+}
