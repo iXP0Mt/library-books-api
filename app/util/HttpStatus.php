2dd455931b5963d359b00ab3feca7b68c14e44f4
iXP0Mt <alex.av.acc@gmail.com>
2025-09-04 16:12:12 +0500

    Added enum http statuses

---
+++ app/util/HttpStatus.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace app\util;
+
+enum HttpStatus: int
+{
+    case OK = 200;
+    case CREATED = 201;
+    case BAD_REQUEST = 400;
+    case UNAUTHORIZED = 401;
+    case FORBIDDEN = 403;
+    case NOT_FOUND = 404;
+    case CONFLICT = 409;
+    case SERVER_ERROR = 500;
+}
