8c728704396af2c3a4e81fbe5e5719b0ddc33f28
iXP0Mt <alex.av.acc@gmail.com>
2025-09-05 14:20:38 +0500

    Added validation JWT

--- app/util/JwtService.php
+++ app/util/JwtService.php
@@ -3,6 +3,7 @@
 namespace app\util;
 
 use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
 
 class JwtService
 {
@@ -15,5 +16,39 @@ class JwtService
         ];
 
         return JWT::encode($payload, $_ENV["JWT_SECRET"], 'HS256');
+    }
+
+    public static function getUserId(string $token): ?int
+    {
+        $payload = [];
+        if(!self::validToken($token, $payload)) {
+            return null;
+        }
+
+        if(!isset($payload['sub'])) {
+            return null;
+        }
+
+        return $payload['sub'];
+    }
+
+    private static function validToken(string $token, array &$payload): bool
+    {
+        $payload = self::decodeToken($token);
+
+        if(!isset($payload['exp'])) {
+            return false;
+        }
+
+        if($payload['exp'] < time()) {
+            return false;
+        }
+
+        return true;
+    }
+
+    private static function decodeToken(string $jwt): array
+    {
+        return (array)JWT::decode($jwt, new Key($_ENV["JWT_SECRET"], 'HS256'));
     }
 }
