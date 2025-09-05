ec1501b7fe3c33d3b5b5157f30436ff8461ee1ea
iXP0Mt <alex.av.acc@gmail.com>
2025-09-05 19:06:08 +0500

    Extends BookDTO

--- app/dto/BookDTO.php
+++ app/dto/BookDTO.php
@@ -5,7 +5,9 @@ namespace app\dto;
 readonly class BookDTO
 {
     public function __construct(
-        public int $bookId,
-        public string $title,
+        public ?int $bookId = null,
+        public ?string $title = null,
+        public ?int $ownerUserId = null,
+        public ?string $text = null,
     ) {}
 }
